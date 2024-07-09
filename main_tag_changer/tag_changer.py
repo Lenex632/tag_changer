e3a7002c37b7add2850aeb929434836f2eab8bcc
Ivan <ivankrasilnikov632@gmail.com>
2024-07-09 17:14:14 +0300

    add find_anime

--- main_tag_changer/tag_changer.py
+++ main_tag_changer/tag_changer.py
@@ -10,11 +10,13 @@ from logger import set_up_logger_config
 
 
 # TODO
-#  Сначала избавляемся от номера
-#  Потом разделяем Title и Artist
-#  Обработали Title и Artist на feat
+#  Сначала избавляемся от номера (check)
+#  Потом разделяем Title и Artist (check)
+#  Обработали Title и Artist на feat (check)
 #  Обработали Title на OP EN скобки
 #  Обработали Title, Album на мусорные скобки
+#  Разделили на Artist и Other (check)
+#  Объединили Title + feat. + OP EN
 class TagChanger:
     def __init__(self, target_dir: str, artist_dirs: list[str]) -> None:
         set_up_logger_config()
@@ -22,36 +24,61 @@ class TagChanger:
         self.target_dir = Path(target_dir)
         self.artist_dirs = artist_dirs
 
-        # цыфры + скобки|пробелы|точки|прочее
+        # цифры + скобки|пробелы|точки|прочее
         self.pattern_to_number = re.compile(r'^\d+(\W | \W | |\)|\) )')
-        self.pattern_to_feat = re.compile(r'(\(|\s)(feat|Feat|ft|Ft)(\. |\.| )(?P<other>.*?)(\)|$)')
+        self.pattern_to_feat = re.compile(r'(\(|\s)(feat|Feat|ft|Ft)(\. |\.| )(?P<feats>.*?)(\)|$)')
+        self.pattern_to_anime = re.compile(r'\((OP|EN)(\d? |\d).*?\)')
 
     def find_numbers(self, target: str) -> str:
         target = self.pattern_to_number.sub('', target)
-        self.logger.debug(f'"{target}"')
+        self.logger.debug(f'{target=}')
 
         return target
 
-    @staticmethod
-    def split_artist(target: str) -> [str, list[str]]:
-        target = target.split(',')
-        other = [s.strip() for s in target]
-        artist = other.pop(0)
+    def split_fullname(self, target: str) -> [str, str]:
+        try:
+            artist, title = target.split(' - ')
+        except ValueError:
+            artist = ''
+            title = target
 
-        return artist, other
+        self.logger.debug(f'{artist=}, {title=}')
+
+        return artist, title
 
     def find_feats(self, target: str) -> [str, list[str]]:
-        artist = target
         feats = []
         match = self.pattern_to_feat.search(target)
 
         if match:
-            artist = self.pattern_to_feat.sub('', target).strip()
-            match = self.pattern_to_feat.search(match.group())
-            feats = match.group('other').split(',')
+            self.logger.debug(f'{match.group()}, {match.group("feats")=}')
+            target = target.replace(match.group(), '').strip()
+            feats = match.group('feats').split(',')
             feats = [feat.strip() for feat in feats]
 
-        return artist, feats
+        self.logger.debug(f'{target=}, {feats=}')
+
+        return target, feats
+
+    def find_anime(self, target: str) -> [str, str]:
+        anime = ''
+        match = self.pattern_to_anime.search(target)
+
+        if match:
+            anime = match.group()
+            target = target.replace(anime, '').strip()
+
+        self.logger.debug(f'{target=}, {anime=}')
+
+        return target, anime
+
+    def split_artist(self, target: str) -> [str, list[str]]:
+        target = target.split(',')
+        other = [s.strip() for s in target]
+        artist = other.pop(0)
+        self.logger.debug(f'{artist=}, {other=}')
+
+        return artist, other
 
 
 if __name__ == '__main__':
--- main_tag_changer/test_tag_changer.py
+++ main_tag_changer/test_tag_changer.py
@@ -23,7 +23,6 @@ class TestClass:
         assert tag_changer.find_numbers('41 - Sum 41 - 41') == 'Sum 41 - 41'
 
     def test_find_numbers_without_artist(self, tag_changer):
-        pass
         assert tag_changer.find_numbers('1. Title') == 'Title'
         assert tag_changer.find_numbers('1)Title') == 'Title'
         assert tag_changer.find_numbers('1) Title') == 'Title'
@@ -44,6 +43,12 @@ class TestClass:
         assert tag_changer.find_numbers('41- 41') == '41'
         assert tag_changer.find_numbers('name w-o number') == 'name w-o number'
         assert tag_changer.find_numbers('name w - o number') == 'name w - o number'
+
+    def test_find_numbers_special(self, tag_changer):
+        # TODO не уверен правильно ли это
+        assert tag_changer.find_numbers('41 Sum') == 'Sum'
+        assert tag_changer.find_numbers('12.Artist - Title') == '12.Artist - Title'
+        assert tag_changer.find_numbers('12 Artist - Title') == 'Artist - Title'
 
     def test_split_artist(self, tag_changer):
         assert tag_changer.split_artist('Artist, Other2, Other3') == ('Artist', ['Other2', 'Other3'])
@@ -78,11 +83,32 @@ class TestClass:
         assert tag_changer.find_feats('Artist(ft. Other)') == ('Artist', ['Other'])
         assert tag_changer.find_feats('Artist(ft.Other)') == ('Artist', ['Other'])
         # несколько исполнителей в feat
-        assert (tag_changer.find_feats('Artist (feat. Other1, Other2, Other3)')
-                == ('Artist', ['Other1', 'Other2', 'Other3']))
-        assert tag_changer.find_feats('Titleft ftOther (feat. Man)') == ('Titleft ftOther', ['Man'])
-        assert tag_changer.find_feats('Titleft (ftOther) (feat. Dave)') == ('Titleft (ftOther)', ['Dave'])
+        assert (tag_changer.find_feats('Artist (feat. Other1, Other2)') == ('Artist', ['Other1', 'Other2']))
+        assert tag_changer.find_feats('Artistft ftOther (feat. Man)') == ('Artistft ftOther', ['Man'])
+        assert tag_changer.find_feats('Artistft (ftOther) (feat. Dave)') == ('Artistft (ftOther)', ['Dave'])
+        # обработка Title
+        assert tag_changer.find_feats('Titleft (feat. Other1, Other2)') == ('Titleft', ['Other1', 'Other2'])
 
+    def test_find_anime(self, tag_changer):
+        assert tag_changer.find_anime('Title (EN One Piece)') == ('Title', '(EN One Piece)')
+        assert tag_changer.find_anime('Title (OP One Piece)') == ('Title', '(OP One Piece)')
+        assert tag_changer.find_anime('Title (OPOne Piece)') == ('Title (OPOne Piece)', '')
+        assert tag_changer.find_anime('Title (OP 1)') == ('Title', '(OP 1)')
+        assert tag_changer.find_anime('Title (EN 1)') == ('Title', '(EN 1)')
+        assert tag_changer.find_anime('Title (OP1)') == ('Title', '(OP1)')
+        assert tag_changer.find_anime('Title (EN1)') == ('Title', '(EN1)')
+        # self.assertEqual((['Artist', 'Title (EN One Piece)'], 'Title (EN One Piece)'), prepare_name(Path('Artist - Title (EN One Piece).mp3')))
+        # self.assertEqual((['Artist', 'Title (OP One Piece)'], 'Title (OP One Piece)'), prepare_name(Path('Artist - Title (OP One Piece).mp3')))
+        # self.assertEqual((['Artist', 'Title'], 'Title'), prepare_name(Path('Artist - Title (OPOne Piece).mp3')))
+        # self.assertEqual((['Artist', 'Title (OP 1)'], 'Title (OP 1)'), prepare_name(Path('Artist - Title (OP 1).mp3')))
+        # self.assertEqual((['Artist', 'Title (OP1)'], 'Title (OP1)'), prepare_name(Path('Artist - Title (OP1).mp3')))
+
+    def test_split_fullname(self, tag_changer):
+        assert tag_changer.split_fullname('Artist - Title') == ('Artist', 'Title')
+        assert tag_changer.split_fullname('Artist - Titleft (feat. Man)') == ('Artist', 'Titleft (feat. Man)')
+        assert tag_changer.split_fullname('Artist - Title (OP One Piece)') == ('Artist', 'Title (OP One Piece)')
+        assert tag_changer.split_fullname('Title') == ('', 'Title')
+        assert tag_changer.split_fullname('name w - o number') == ('name w', 'o number')
 
 
 
@@ -91,22 +117,12 @@ class TestClass:
     #
     #     self.assertEqual((['Artist', 'Title'], 'Title'), prepare_name(Path('Artist - Title.mp3')))
     #     self.assertEqual((['Artist', 'Title'], 'Title'), prepare_name(Path('Artist - Title (New edition).mp3')))
-    #     self.assertEqual((['Artist', 'Titleft (feat. Man)'], 'Titleft (feat. Man)'),
-    #                      prepare_name(Path('Artist - Titleft (feat. Man).mp3')))
-    #     self.assertEqual((['Artist', 'Title (ft. Man)'], 'Title (ft. Man)'),
-    #                      prepare_name(Path('Artist - Title (ft. Man).mp3')))
-    #     self.assertEqual((['Artist', 'Title(feat. Man)'], 'Title(feat. Man)'),
-    #                      prepare_name(Path('Artist - Title(feat. Man).mp3')))
+    #     self.assertEqual((['Artist', 'Titleft (feat. Man)'], 'Titleft (feat. Man)'), prepare_name(Path('Artist - Titleft (feat. Man).mp3')))
+    #     self.assertEqual((['Artist', 'Title (ft. Man)'], 'Title (ft. Man)'), prepare_name(Path('Artist - Title (ft. Man).mp3')))
+    #     self.assertEqual((['Artist', 'Title(feat. Man)'], 'Title(feat. Man)'), prepare_name(Path('Artist - Title(feat. Man).mp3')))
     #     self.assertEqual((['Artist', 'Title'], 'Title'), prepare_name(Path('Artist - Title(New edition).mp3')))
     #     self.assertEqual((['Artist', 'Title'], 'Title'), prepare_name(Path('Artist - Title(featmen).mp3')))
     #     self.assertEqual((['Artist', 'Title'], 'Title'), prepare_name(Path('Artist - Title(FeatMen).mp3')))
-    #     self.assertEqual((['Artist', 'Title (EN One Piece)'], 'Title (EN One Piece)'),
-    #                      prepare_name(Path('Artist - Title (EN One Piece).mp3')))
-    #     self.assertEqual((['Artist', 'Title (OP One Piece)'], 'Title (OP One Piece)'),
-    #                      prepare_name(Path('Artist - Title (OP One Piece).mp3')))
-    #     self.assertEqual((['Artist', 'Title'], 'Title'), prepare_name(Path('Artist - Title (OPOne Piece).mp3')))
-    #     self.assertEqual((['Artist', 'Title (OP 1)'], 'Title (OP 1)'), prepare_name(Path('Artist - Title (OP 1).mp3')))
-    #     self.assertEqual((['Artist', 'Title (OP1)'], 'Title (OP1)'), prepare_name(Path('Artist - Title (OP1).mp3')))
     #     self.assertEqual((['Artist', 'The (S)hift'], 'The (S)hift'), prepare_name(Path('Artist - The (S)hift.mp3')))
     #     self.assertEqual((['Artist', 'The Sh(i)ft'], 'The Sh(i)ft'), prepare_name(Path('Artist - The Sh(i)ft.mp3')))
     #

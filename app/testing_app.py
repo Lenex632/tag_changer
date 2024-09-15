632edb4e87716e01459de67d4d5051a9de9af27d
lenex <ivankrasilnikov632@gmail.com>
2024-09-15 17:20:53 +0300

    up app ui

--- app/testing_app.py
+++ app/testing_app.py
@@ -13,7 +13,112 @@ from PyQt6.QtWidgets import (
     QDialog,
     QFileDialog,
     QTextEdit,
+    QLayout,
 )
+
+
+class TargetDir(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.placeholder = 'Укажите путь к исходной папке'
+
+        self.label = QLabel(self.placeholder)
+        self.button = self.create_button()
+        self.fild = self.create_fild()
+
+        self.create_layout()
+
+    def create_button(self) -> QPushButton:
+        button = QPushButton('O')
+        button.clicked.connect(self.click_button)
+
+        return button
+
+    def click_button(self) -> None:
+        dialog = QFileDialog()
+        dialog.setWindowTitle(self.placeholder)
+        chosen_dir = dialog.getExistingDirectory()
+        self.fild.setText(chosen_dir)
+
+    def create_fild(self) -> QTextEdit:
+        fild = QTextEdit()
+        fild.setPlaceholderText(self.placeholder)
+
+        return fild
+
+    def create_layout(self) -> None:
+        bottom_layout = QHBoxLayout()
+        bottom_layout.addWidget(self.button)
+        bottom_layout.addWidget(self.fild)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addLayout(bottom_layout)
+
+        self.setLayout(layout)
+
+
+class ArtistDir(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.placeholder = 'Укажите папки с исполнителями для Уровня 2 (через запятую)'
+
+        self.label = QLabel(self.placeholder)
+        self.fild = self.create_fild()
+
+        self.create_layout()
+
+    def create_fild(self) -> QTextEdit:
+        fild = QTextEdit()
+        fild.setPlaceholderText(self.placeholder)
+
+        return fild
+
+    def create_layout(self) -> None:
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.fild)
+
+        self.setLayout(layout)
+
+
+class MainButton(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.readme_button = self.create_button('Открыть ReadMe', self.open_readme)
+        self.reset_settings_button = self.create_button('Сбросить настройки', self.reset_settings)
+        self.save_settings_button = self.create_button('Сохранить настройки', self.save_settings)
+        self.start_button = self.create_button('Запуск   >>', self.start)
+
+        self.create_layout()
+
+    def create_button(self, name: str, func: callable) -> QPushButton:
+        button = QPushButton(name)
+        button.clicked.connect(func)
+
+        return button
+
+    def create_layout(self):
+        layout = QGridLayout()
+        layout.addWidget(self.readme_button, 0, 0)
+        layout.addWidget(self.reset_settings_button, 1, 0)
+        layout.addWidget(self.save_settings_button, 1, 1)
+        layout.addWidget(self.start_button, 1, 2)
+
+        self.setLayout(layout)
+
+    def open_readme(self):
+        print('Открыть ReadMe')
+
+    def reset_settings(self):
+        print('Сбросить настройки')
+
+    def save_settings(self):
+        print('Сохранить настройки')
+
+    def start(self):
+        print('Запуск   >>')
 
 
 class MainWindow(QMainWindow):
@@ -21,11 +126,8 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.set_window_parameters()
-
         self.main_layout = QVBoxLayout()
-        self.create_target_dir_layout()
-        self.create_artist_dir_layout()
-        self.create_main_buttons_layout()
+        self.create_main_layout()
 
         widget = QWidget()
         widget.setLayout(self.main_layout)
@@ -37,35 +139,14 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Tag Changer')
         self.setFixedSize(QSize(800, 500))
 
-    def create_target_dir_layout(self):
-        layout = QVBoxLayout()
-        label = QLabel('Укажите путь к исходной папке')
-        fild = QTextEdit('Enter dir name')
-        layout.addWidget(label)
-        layout.addWidget(fild)
-        self.main_layout.addLayout(layout)
+    def create_main_layout(self):
+        target_dir_widget = TargetDir()
+        artist_dir_widget = ArtistDir()
+        main_button_widget = MainButton()
 
-    def create_artist_dir_layout(self):
-        layout = QVBoxLayout()
-        label = QLabel('Укажите папки с исполнителями для Уровня 2 (через запятую)')
-        fild = QTextEdit('Enter dirs name')
-        layout.addWidget(label)
-        layout.addWidget(fild)
-        self.main_layout.addLayout(layout)
-
-    def create_main_buttons_layout(self):
-        readme_button = QPushButton('Открыть ReadMe')
-        reset_settings_button = QPushButton('Сбросить настройки')
-        save_settings_button = QPushButton('Сохранить настройки')
-        start_button = QPushButton('Запуск   >>')
-
-        layout = QGridLayout()
-        layout.addWidget(readme_button, 0, 0)
-        layout.addWidget(reset_settings_button, 1, 0)
-        layout.addWidget(save_settings_button, 1, 1)
-        layout.addWidget(start_button, 1, 2)
-
-        self.main_layout.addLayout(layout)
+        self.main_layout.addWidget(target_dir_widget)
+        self.main_layout.addWidget(artist_dir_widget)
+        self.main_layout.addWidget(main_button_widget)
 
 
 if __name__ == '__main__':

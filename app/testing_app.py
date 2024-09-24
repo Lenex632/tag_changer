a85fe97b0353c01d289974fc6c3238817ced8004
lenex <ivankrasilnikov632@gmail.com>
2024-09-24 16:40:00 +0300

    update app ui set and get settings

--- app/testing_app.py
+++ app/testing_app.py
@@ -1,5 +1,6 @@
-import sys
 import configparser
+from pathlib import Path
+import sys
 
 from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import (
@@ -16,9 +17,43 @@ from PyQt6.QtWidgets import (
 )
 
 
-class TargetDir(QWidget):
+class Settings:
     def __init__(self):
+        self._settings = configparser.ConfigParser()
+        self.path = Path(Path(__file__).parent.parent, 'settings.ini')
+        if self.path:
+            self._settings.read(self.path)
+
+        self.target_dir = None
+        self.artist_dirs = None
+        self.set_defaults()
+
+    def set_defaults(self) -> None:
+        self.target_dir = self._settings.get(section='settings', option='target_dir', fallback=None)
+        self.artist_dirs = self._settings.get(section='settings', option='artist_dirs', fallback='').split('\n')
+
+    def set_target_dir(self, value) -> None:
+        self._settings.set(section='settings', option='target_dir', value=value)
+        self.target_dir = value
+
+    def set_artist_dir(self, value) -> None:
+        self._settings.set(section='settings', option='artist_dirs', value=value)
+        self.artist_dirs = value
+
+    def save_settings(self) -> None:
+        with open(self.path, 'w') as file:
+            self._settings.write(file)
+
+    def clean_data(self) -> None:
+        self.set_target_dir('')
+        self.set_artist_dir('')
+        self.save_settings()
+
+
+class TargetDir(QWidget):
+    def __init__(self, settings):
         super().__init__()
+        self.settings = settings
         self.placeholder = 'Укажите путь к исходной папке'
 
         self.label = QLabel(self.placeholder)
@@ -42,6 +77,8 @@ class TargetDir(QWidget):
     def create_fild(self) -> QTextEdit:
         fild = QTextEdit()
         fild.setPlaceholderText(self.placeholder)
+        if self.settings.target_dir:
+            fild.setText(self.settings.target_dir)
 
         return fild
 
@@ -57,10 +94,11 @@ class TargetDir(QWidget):
         self.setLayout(layout)
 
 
-class ArtistDir(QWidget):
-    def __init__(self):
+class ArtistDirs(QWidget):
+    def __init__(self, settings):
         super().__init__()
-        self.placeholder = 'Укажите папки с исполнителями для Уровня 2 (через запятую)'
+        self.settings = settings
+        self.placeholder = 'Укажите папки с исполнителями для Уровня 2 (каждый с новой строки)'
 
         self.label = QLabel(self.placeholder)
         self.fild = self.create_fild()
@@ -70,6 +108,9 @@ class ArtistDir(QWidget):
     def create_fild(self) -> QTextEdit:
         fild = QTextEdit()
         fild.setPlaceholderText(self.placeholder)
+        if self.settings.artist_dirs:
+            text = '\n'.join(self.settings.artist_dirs)
+            fild.setText(text)
 
         return fild
 
@@ -82,8 +123,9 @@ class ArtistDir(QWidget):
 
 
 class MainButton(QWidget):
-    def __init__(self):
+    def __init__(self, settings):
         super().__init__()
+        self.settings = settings
 
         self.readme_button = self.create_button('Открыть ReadMe', self.open_readme)
         self.reset_settings_button = self.create_button('Сбросить настройки', self.reset_settings)
@@ -111,6 +153,7 @@ class MainButton(QWidget):
         print('Открыть ReadMe')
 
     def reset_settings(self):
+        self.settings.clean_data()
         print('Сбросить настройки')
 
     def save_settings(self):
@@ -123,6 +166,7 @@ class MainButton(QWidget):
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.settings = Settings()
 
         self.set_window_parameters()
         self.main_layout = QVBoxLayout()
@@ -139,20 +183,16 @@ class MainWindow(QMainWindow):
         self.setFixedSize(QSize(800, 500))
 
     def create_main_layout(self):
-        target_dir_widget = TargetDir()
-        artist_dir_widget = ArtistDir()
-        main_button_widget = MainButton()
+        target_dir_widget = TargetDir(self.settings)
+        artist_dirs_widget = ArtistDirs(self.settings)
+        main_button_widget = MainButton(self.settings)
 
         self.main_layout.addWidget(target_dir_widget)
-        self.main_layout.addWidget(artist_dir_widget)
+        self.main_layout.addWidget(artist_dirs_widget)
         self.main_layout.addWidget(main_button_widget)
 
 
 if __name__ == '__main__':
-    # app = QApplication(sys.argv)
-    # window = MainWindow()
-    # app.exec()
-    config = configparser.ConfigParser()
-    config.read('../settings.ini')
-    print(config['settings']['target_dir'])
-    print(config.get('settings', 'artist_dir').split('\n'))
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    app.exec()
